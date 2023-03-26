89db0ac3216022ea68161665110503c0803a3bdb
taashev <taashev.a.r92@yandex.ru>
2023-03-26 22:55:50 +0300

    feat: add firebase remote config

--- components/App/App.js
+++ components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {
   SafeAreaView,
@@ -6,15 +6,34 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  View,
 } from 'react-native';
 
+import remoteConfig from '@react-native-firebase/remote-config';
+
 export function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isConnected, setIsConnected] = useState(true);
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    remoteConfig()
+      .setDefaults({url: ''})
+      .then(() => remoteConfig().fetch(0))
+      .then(() => remoteConfig().fetchAndActivate())
+      .then(() => {
+        const getUrl = remoteConfig().getString('url');
+        // AsyncStorage.setItem('url', getUrl);
+        setUrl(getUrl);
+      })
+      .catch(e => console.log(e))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
       <ScrollView>
-        <Text>Hello</Text>
+        <Text>{url}</Text>
       </ScrollView>
     </SafeAreaView>
   );
